16ccd003193e1e3772ae332c74bd0cc426a58554
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-04-20 06:32:40 +0000

    adjusting style.css and game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-<script>
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
@@ -7,4 +7,3 @@ ctx.rect(20, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
-</script>
